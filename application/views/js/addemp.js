a44412f4efdc009e13969ebd3f3f5d7198c058f6
subhankartier5 <roysubho687@gmail.com>
2016-01-27 14:19:16 +0530

    validation complete in add employee file and comment out for future ref

--- application/views/js/addemp.js
+++ application/views/js/addemp.js
@@ -1,38 +1,46 @@
 
 $(document).ready(function(){
-	$('#errorAdd').html('');
-	$('#confirmAdd').html('');
+	$('#errorAdd').html(''); //making divs empty after page ready
+	$('#confirmAdd').html(''); //making divs empty after page ready
 		$('#addemp').click(function(){
+			var emppass= $('#emppass').val(); //taking values
+	   		var emppass=$.trim(emppass);		//removing spaces
+	   		var submitadd= $('#addemp').val();
 	   		var empname= $('#empname').val();
 	   		var empname=$.trim(empname);
+	   		//checking proper email type
 	   		var filter= /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
-	   		var empemail= $('#empemail').val();	   	  
-	   		var empemail= filter.test(empemail);
-	   		if (empemail== true && empemail.length != 0) 
+	   		//taking value of email
+	   		var empemail= $('#empemail').val();
+	   		var empemail=$.trim(empemail);
+	   		if (empname && empemail && emppass) //if all fields exist
 	   		{
-	   			
+	   			$('#errorAdd').html('');	//making error div blank 
+	   			var empemail= filter.test(empemail); //validating email
+	   		
+	   		if (empemail== true) //if email in proper way exist
+	   		{
+	   			//take this new email value
 	   			var empemail= $('#empemail').val();
-	   			var empemail=$.trim(empemail);
+	   			var empemail=$.trim(empemail); //trim this i.e. rmv spaces
 
 	   			
 	   		}
-	   		else //if (empemail==false && empemail.length = 0) 
+	   		else //if email in proper way doesnt exist
 	   		{	
 	   				
 	   				
 	   				$('#errorAdd1').html('*invalid email format, email should be like email@domain.com');
-	   			
+	   				
 	   		}
-
-	   		var emppass= $('#emppass').val();
-	   		var emppass=$.trim(emppass);
-	   		var submitadd= $('#addemp').val();
+	   	
+	   		
 	   			
-	   		if (empname && empemail && emppass) 
+	   		if (empname && empemail && emppass) //now again a crosscheck if all fields exist in a proper manner
 	   		{
 	   			$.post('Admin/addEmp',{name: empname, email: empemail, pass: emppass, btn: submitadd}, function(data){
                 $('#confirmAdd').html(data);
-                  			//making fileds blank after submit
+                //making fileds blank after submit
                 $('#empname').val('');
                 $('#empemail').val('');
                 $('#emppass').val('');
@@ -41,17 +49,24 @@ $(document).ready(function(){
                 });
 	   					
 	   		}
-	   		else 
+	   		else //if doesnot in proper manner
 	   		{	
 	   			
 	   				$('#confirmAdd').html('');
-	   				$('#errorAdd').html('*All Fields are Mandetory');
+	   				
 	   				$('#empname').focus();
 
 	   			
 
 	   				
 	   		}
+	   	}
+	   	else //if all fields doesnt exist
+	   	{	
+	   		$('#errorAdd1').html('');
+	   		$('#errorAdd').html('*All Fields are Mandetory');
+	   	}
+
 	   		
 	   		
 
