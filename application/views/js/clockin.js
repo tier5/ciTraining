f5561c056257761983bbff177047f6be7f3a99bf
subhankartier5 <roysubho687@gmail.com>
2016-01-27 19:27:41 +0530

    worked at employee dashboard added clockin clockout and break status

---
+++ application/views/js/clockin.js
@@ -0,0 +1,32 @@
+ $(document).ready(function(){
+        $("#clockbtn").click(function(){
+
+            var btn=$('#clockbtn').text();
+            //alert(btn);
+            if (btn === 'Clock Out') 
+            {
+                $.post('Employee/clockin', {btn: btn}, function(data){
+
+                $('#clockintime').html(data);
+                $('#clockintime1').html('Hey! Guest, have a nice day!');
+
+
+           });
+        }
+        else
+            {
+                $('#clockintime1').html('Hope! You have enjoyed your office');
+
+                 $.post('Employee/clockout', {btn: btn}, function(data){
+
+                $('#clockintime').html(data);
+                $('#clockintime1').html('Hey! Guest, see you tommorow');
+
+
+           });
+            }
+
+        });
+
+
+    });
