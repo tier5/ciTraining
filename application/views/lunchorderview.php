4d4188233e6db45350815e9e102de525fe57ec13
sudiptamitra <sudiptamit1@gmail.com>
2016-03-17 11:30:22 +0530

    make minor changes in js fiel

--- application/views/lunchorderview.php
+++ application/views/lunchorderview.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php include 'lateviewheader.php';?>
+<?php include 'lunchviewheader.php';?>
 
 <!-- The #page-top ID is part of the scrolling feature - the data-spy and data-target are part of the built-in Bootstrap scrollspy function -->
 <body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
