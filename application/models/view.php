dae5584268d3e5792f388b00e6eda9583ec9e251
sudiptamitra <sudiptamit1@gmail.com>
2016-03-22 13:50:32 +0530

    reset master branch

--- application/models/view.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<?php include 'employeeheader.php';?>
-<p>Date: <input type="text" id="datepicker"></p>
-<button id="date">DatePicker</button>
-<body>
-</body>
-</html>
--- application/views/js/usercalender.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-//var a=0;
-
-});
